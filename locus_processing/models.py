e92fe9f49294e0e64993670f730dfc2daedc31f2
Sander Bollen <a.h.b.bollen@lumc.nl>
2017-07-25 11:10:03 +0200

    check transcript names

--- locus_processing/models.py
+++ locus_processing/models.py
@@ -115,8 +115,9 @@ class Snp(object):
             )
             return res
         transcripts = position_response.json()
-        if transcript is not None and transcript in transcripts:
-            tr = transcripts[transcripts.index(transcript)]
+        transcript_names = list(map(lambda x: x.split(":")[0], transcripts))
+        if transcript is not None and transcript in transcript_names:
+            tr = transcripts[transcript_names.index(transcript)]
         elif transcript is not None:
             res.warnings.append("Could not find preferred transcript")
             tr = transcripts[0]
